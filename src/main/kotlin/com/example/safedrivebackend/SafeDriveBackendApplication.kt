8cce9b184d8865e305cead2aad61e6a77476a827
Dilan Nawarathna <124450203+hashdila@users.noreply.github.com>
2024-12-05 11:17:38 +0900

    Crete app

---
+++ src/main/kotlin/com/example/safedrivebackend/SafeDriveBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.safedrivebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SafeDriveBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<SafeDriveBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/safedrivebackend/SafeDriveBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.safedrivebackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SafeDriveBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
